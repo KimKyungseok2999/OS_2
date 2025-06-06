888ae03b66795f3d8ddb3d900d7129042977c99d
김경석 <0525kimkss@naver.com>
2025-06-06 20:05:06 +0900

    0605+0606 작성
    1. range에 복사하고 새로운 큐에넣는과정을 작성 큐에넣고 메모리해제 추가.
    2. enq에서 기존노드를 덮어 썼다면 미리 만들어둔 inode를 사용하지 않았으니 delete
    3. nfree 작성 (노드내부 메모리해제, 자체 메모리해제)
    ++delete만으로는 item.value가 가리키는 동적 메모리 해제가 안되서 그냥 작성
    4. flag를 이용해 스핀락 구현 + eq,dq,range에 락 언락 추가

---
+++ qtype.h
@@ -0,0 +1,47 @@
+﻿#ifndef _QTYPE_H  // header guard
+#define _QTYPE_H
+#include <atomic>
+
+// ==========이 파일은 수정 가능==========
+
+typedef unsigned int Key;  // 값이 클수록 높은 우선순위
+typedef void* Value;
+
+typedef struct {
+    Key key;
+    Value value;
+    int value_size; // byte
+} Item;
+
+typedef struct {
+    bool success;   // true: 성공, false: 실패
+    Item item;
+    // 필드 추가 가능
+} Reply;
+
+typedef struct node_t {
+    Item item;
+    struct node_t* next;
+    // 필드 추가 가능
+} Node;
+
+//flag를 이용한 스핀락 구현
+typedef struct SpinLock {
+    std::atomic_flag flag = ATOMIC_FLAG_INIT;
+
+    void lock() {
+        while (flag.test_and_set(std::memory_order_acquire)) {}
+    }
+
+    void unlock() {
+        flag.clear(std::memory_order_release);
+    }
+} SpinLock;
+
+typedef struct {
+    Node* head, *tail;
+    SpinLock lock;
+    // 필드 추가 가능
+} Queue;
+
+#endif
--- queue.cpp
+++ queue.cpp
@@ -16,7 +16,7 @@ void release(Queue* queue) {
 	//노드가 많으니 반복문
 	while (free) {
 		Node* nextfree = free->next;
-		delete free; //얘만 nfree에 둘까..?
+		nfree(free);
 		free = nextfree;
 	}
 }
@@ -32,7 +32,11 @@ Node* nalloc(Item item) {
 
 
 void nfree(Node* node) {
-	return;
+	if (node != nullptr) {
+		if (node->item.value != nullptr)
+			delete (int*)(node->item.value);
+		delete node;
+	}
 }
 
 //원본을 삭제해도 복사본이 남게 깊은복사를 해라..?
@@ -67,6 +71,7 @@ Node* nclone(Node* node) {
 
 // 키가 이미 존재하면 덮어쓰기를 해라..?
 Reply enqueue(Queue* queue, Item item) {
+	queue->lock.lock();
 	Reply reply = { false, NULL };
 	Node* inode = nalloc(item); //일단 노드 생성 하고
 
@@ -82,8 +87,15 @@ Reply enqueue(Queue* queue, Item item) {
 			// 여기다 조건을 하나 더 넣어야함
 			//(key=10, value="abc"), (key=10, value="xyz")가 차례로 수행된다면, 즉 키가 같다면
 			if (now->item.key == item.key) {
+
+				if (now->item.value)
+					delete (int*)now->item.value;
+
 				//value만 덮어써라
 				now->item.value = item.value;
+				//inode생성을 했는데 기존노드만 덮어쓴거면 지워야하지 않나..
+				delete inode;
+				queue->lock.unlock();
 				return { true, item };
 			}
 			bef = now;
@@ -100,13 +112,16 @@ Reply enqueue(Queue* queue, Item item) {
 	}
 	reply.success = true; //연산 수행완료
 	reply.item = item;
+	queue->lock.unlock();
 	return reply;
 }
 
 Reply dequeue(Queue* queue) {
+	queue->lock.lock();
 	if (queue->head == nullptr) 
 	{
 		Reply reply = { false, NULL };
+		queue->lock.unlock();
 		return reply;
 	}
 	Node* del = queue->head;
@@ -115,6 +130,7 @@ Reply dequeue(Queue* queue) {
 	Item a = del->item;
 	delete del;
 	Reply reply = { true, a };
+	queue->lock.unlock();
 	return reply;
 }
 
@@ -122,14 +138,18 @@ Reply dequeue(Queue* queue) {
 Queue* range(Queue* queue, Key start, Key end) {
 	//새로운 큐 만들고 처음노드 시작.
 	Queue* nq = init();
+	queue->lock.lock();
 	Node* now = queue->head;
 	//반복
 	while (now) {
 		// 현재 노드가 키의 범위
 		if (now->item.key >= start && now->item.key <= end) {
-			nclone(now); //깊은복사
+			Node* copy = nclone(now); //깊은복사
+			enqueue(nq, copy->item);
+			nfree(copy);
 		}
 		now = now->next;
 	}
+	queue->lock.unlock();
 	return nq;
 }
